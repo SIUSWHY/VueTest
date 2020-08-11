5dbe56ff5fbac35f07a13c105d5b3b96f9dea419
Daniil <ventsovdaniil@yandex.ru>
2020-08-11 14:58:45 +0300

    some fix v.3

---
+++ src/components/vuex/store.js
@@ -0,0 +1,13 @@
+import vue from 'vue'
+import vuex from 'vuex'
+
+vue.use(vuex);
+
+let store = new vuex.Store(options:{
+state:{},
+mutation :{},
+actions:{},
+getters:{}
+});
+
+export default store;
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
+// import Store from './vuex/store'
 
 Vue.config.productionTip = false
 
